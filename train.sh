b87374d32e494a48af029e7753b7914025771956
Oleg Kariuk <olegkariuk@gmail.com>
2019-10-31 09:38:59 +0200

    Fixes installation errors

---
+++ train.sh
@@ -0,0 +1,25 @@
+python -m bin.train \
+  --config_paths="
+    $NMT_CONFIGS/nmt_medium.yml,
+    $NMT_CONFIGS/train_seq2seq.yml,
+    $NMT_CONFIGS/text_metrics_bpe.yml" \
+  --model_params "
+      vocab_source: $VOCAB_SOURCE
+      vocab_target: $VOCAB_TARGET" \
+  --input_pipeline_train "
+    class: ParallelTextInputPipeline
+    params:
+      source_files:
+        - $TRAIN_SOURCES
+      target_files:
+        - $TRAIN_TARGETS" \
+  --input_pipeline_dev "
+    class: ParallelTextInputPipeline
+    params:
+       source_files:
+        - $DEV_SOURCES
+       target_files:
+        - $DEV_TARGETS" \
+  --batch_size 32 \
+  --train_steps $TRAIN_STEPS \
+  --output_dir "$MODEL_DIR"
